7351e539ad56f91e3522761b49118b05ceff22bc
shyamilic25 <shyamilitc@gmail.com>
2025-01-05 16:29:07 +0530

    removed knowmore

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./styles.css";
 import App from "./App.jsx";
 import Header from "./header";
-import KnowMore from "./KnowMore";
+// import KnowMore from "./KnowMore";
 import PortFolio from "./PortFolio";
 
 createRoot(document.getElementById("root")).render(
@@ -12,7 +12,7 @@ createRoot(document.getElementById("root")).render(
     <Router>
       <Routes>
         <Route path="/" element={<App />} />
-        <Route path="/knowmore" element={<KnowMore />} />
+        {/* <Route path="/knowmore" element={<KnowMore />} /> */}
         <Route path="/portfolio" element={<PortFolio />} />
       </Routes>
     </Router>
