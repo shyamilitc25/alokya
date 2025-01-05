7405194ffcaef283e030f11f6aa80792d06130ab
shyamilic25 <shyamilitc@gmail.com>
2025-01-05 16:04:07 +0530

    new commits

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./styles.css";
 import App from "./App.jsx";
 import Header from "./header";
-import Portfolio from "./Portfolio.jsx"; // Correct file reference
+import Portfolio from "./PortFolio.jsx"; // Correct file reference
 import KnowMore from "./KnowMore.jsx";
 
 createRoot(document.getElementById("root")).render(
