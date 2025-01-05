67c694d0c626373ff17ae3118b9fc4d15bb08021
shyamilic25 <shyamilitc@gmail.com>
2025-01-05 16:34:05 +0530

    changed file name

--- src/main.jsx
+++ src/main.jsx
@@ -4,15 +4,15 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./styles.css";
 import App from "./App.jsx";
 import Header from "./header";
-// import KnowMore from "./KnowMore";
-import PortFolio from "./PortFolio";
+import KnowMore from "./Knowmore";
+import PortFolio from "./Portfolio";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Router>
       <Routes>
         <Route path="/" element={<App />} />
-        {/* <Route path="/knowmore" element={<KnowMore />} /> */}
+        <Route path="/knowmore" element={<KnowMore />} />
         <Route path="/portfolio" element={<PortFolio />} />
       </Routes>
     </Router>
